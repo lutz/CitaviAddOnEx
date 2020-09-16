2bd850ea04107ef3ab36a10ad87c153b743cdf85
Daniel Lutz <daniel.lutz@outlook.com>
2020-09-16 14:39:54 +0200

    restructure code add fluent Update/Create extensions methods to Commandbar and CommendbarMenu

---
+++ src/CitaviAddOnEx/Core/CommandbarFluentEx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace SwissAcademic.Controls
+{
+    public static class CommandbarFluentEx
+    {
+        public static Commandbar CreateCommandbarMenu(this Commandbar commandbar, string key, string text, Action<CommandbarMenu> createdAction)
+        {
+            var menu = commandbar.AddCommandbarMenu(key, text);
+            if (menu != null)
+            {
+                createdAction?.Invoke(menu);
+            }
+            return commandbar;
+        }
+
+        public static Commandbar CreateCommandbarButton(this Commandbar commandbar, string key, string text, Action<CommandbarButton> createdAction)
+        {
+            var button = commandbar.AddCommandbarButton(key, text);
+            if (button != null)
+            {
+                createdAction?.Invoke(button);
+            }
+            return commandbar;
+        }
+
+        public static Commandbar CreateCommandbarMenu(this Commandbar commandbar, int index, string key, string text, Action<CommandbarMenu> createdAction)
+        {
+            var menu = commandbar.InsertCommandbarMenu(index, key, text);
+            if (menu != null)
+            {
+                createdAction?.Invoke(menu);
+            }
+            return commandbar;
+        }
+
+        public static Commandbar CreateCommandbarButton(this Commandbar commandbar, int index, string key, string text, Action<CommandbarButton> createdAction)
+        {
+            var button = commandbar.InsertCommandbarButton(index, key, text);
+            if (button != null)
+            {
+                createdAction?.Invoke(button);
+            }
+            return commandbar;
+        }
+
+        public static Commandbar UpdateCommandbarMenu(this Commandbar commandbar, string key, Action<CommandbarMenu> updateAction)
+        {
+            var menu = commandbar.GetCommandbarMenu(key);
+            if (menu != null)
+            {
+                updateAction?.Invoke(menu);
+            }
+            return commandbar;
+        }
+
+        public static Commandbar UpdateCommandbarButton(this Commandbar commandbar, string key, Action<CommandbarButton> updateAction)
+        {
+            var button = commandbar.GetCommandbarButton(key);
+            if (button != null)
+            {
+                updateAction?.Invoke(button);
+            }
+            return commandbar;
+        }
+    }
+}
---
+++ src/CitaviAddOnEx/Core/CommandbarMenuFluentEx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace SwissAcademic.Controls
+{
+    public static class CommandbarMenuFluentEx
+    {
+        public static CommandbarMenu CreateCommandbarMenu(this CommandbarMenu commandbarMenu, string key, string text, Action<CommandbarMenu> createdAction)
+        {
+            var menu = commandbarMenu.AddCommandbarMenu(key, text);
+            if (menu != null)
+            {
+                createdAction?.Invoke(menu);
+            }
+            return commandbarMenu;
+        }
+
+        public static CommandbarMenu CreateCommandbarButton(this CommandbarMenu commandbarMenu, string key, string text, Action<CommandbarButton> createdAction)
+        {
+            var button = commandbarMenu.AddCommandbarButton(key, text);
+            if (button != null)
+            {
+                createdAction?.Invoke(button);
+            }
+            return commandbarMenu;
+        }
+
+        public static CommandbarMenu CreateCommandbarMenu(this CommandbarMenu commandbarMenu, int index, string key, string text, Action<CommandbarMenu> createdAction)
+        {
+            var menu = commandbarMenu.InsertCommandbarMenu(index, key, text);
+            if (menu != null)
+            {
+                createdAction?.Invoke(menu);
+            }
+            return commandbarMenu;
+        }
+
+        public static CommandbarMenu CreateCommandbarButton(this CommandbarMenu commandbarMenu, int index, string key, string text, Action<CommandbarButton> createdAction)
+        {
+            var button = commandbarMenu.InsertCommandbarButton(index, key, text);
+            if (button != null)
+            {
+                createdAction?.Invoke(button);
+            }
+            return commandbarMenu;
+        }
+
+        public static CommandbarMenu UpdateCommandbarMenu(this CommandbarMenu commandbarMenu, string key, Action<CommandbarMenu> updateAction)
+        {
+            var menu = commandbarMenu.GetCommandbarMenu(key);
+            if (menu != null)
+            {
+                updateAction?.Invoke(menu);
+            }
+            return commandbarMenu;
+        }
+
+        public static CommandbarMenu UpdateCommandbarButton(this CommandbarMenu commandbarMenu, string key, Action<CommandbarButton> updateAction)
+        {
+            var button = commandbarMenu.GetCommandbarButton(key);
+            if (button != null)
+            {
+                updateAction?.Invoke(button);
+            }
+            return commandbarMenu;
+        }
+    }
+
+}
