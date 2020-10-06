0ebf1cb1acbf07360fe7884f139b58c97b2cde9b
Daniel Lutz <daniel.lutz@outlook.com>
2020-10-06 11:26:45 +0200

    add testaddon

---
+++ src/TestAddon/Addon.cs
@@ -0,0 +1,21 @@
+﻿using SwissAcademic;
+using SwissAcademic.Citavi;
+using SwissAcademic.Citavi.Shell;
+using SwissAcademic.Controls;
+using SwissAcademic.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestAddon
+{
+    public class Addon : CitaviAddOnEx<MacroEditorForm>
+    {
+        public override void OnHostingFormLoaded(MacroEditorForm form)
+        {
+          
+        }
+    }
+}
